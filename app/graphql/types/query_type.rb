490a4e454bd175a31ae8d33eb3bdc48712f9b549
Dan Bota <dan.cristian.bota@gmail.com>
2021-02-08 11:49:17 +0200

    What’s in the Martian Library?
    rails g graphql:object item + revisit type definition
    rails g graphql:object user + revisit type definition and add the full_name computed field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,11 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
+    field :items, [Types::ItemType],
+          null: false,
+          description: "Returns a list of items in the martian library"
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def items
+      Item.all
     end
   end
 end
