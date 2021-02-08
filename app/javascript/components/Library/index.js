9f11b3a5e1be925d3b94846c9fc7bec22ba6a1bb
Dan Bota <dan.cristian.bota@gmail.com>
2021-02-08 15:48:43 +0200

    UpdateItemForm

--- app/javascript/components/Library/index.js
+++ app/javascript/components/Library/index.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
-import { Query } from "react-apollo";
-import { LibraryQuery } from "./operations.graphql";
+import React, {useState} from "react";
+import {Query} from "react-apollo";
+import {LibraryQuery} from "./operations.graphql";
 import cs from "./styles";
+import UpdateItemForm from "../UpdateItemForm";
 
-const ConsoleLog = ({ children }) => {
+const ConsoleLog = ({children}) => {
     console.log(children);
     return false;
 };
@@ -12,25 +13,36 @@ const Library = () => {
     const [item, setItem] = useState(null);
     return (
         <Query query={LibraryQuery}>
-            {({ data, loading }) => (
+            {({data, loading}) => (
                 <div className={cs.library}>
                     <ConsoleLog>{data}</ConsoleLog>
                     {loading || !data.items
                         ? "loading..."
-                        : data.items.map(({ title, id, user, imageUrl, description }) => (
+                        : data.items.map(({title, id, user, imageUrl, description}) => (
                             <button
                                 key={id}
                                 className={cs.plate}
-                                onClick={() => setItem({ title, imageUrl, id, description })}
+                                onClick={() => setItem({title, imageUrl, id, description})}
                             >
                                 <div className={cs.title}>{title}</div>
                                 <div>{description}</div>
-                                {imageUrl && <img src={imageUrl} className={cs.image} />}
+                                {imageUrl && <img src={imageUrl} className={cs.image}/>}
                                 {user ? (
                                     <div className={cs.user}>added by {user.email}</div>
                                 ) : null}
                             </button>
                         ))}
+                    {
+                        item !== null && (
+                            <UpdateItemForm
+                                id={item.id}
+                                initialTitle={item.title}
+                                initialDescription={item.description}
+                                initialImageUrl={item.imageUrl}
+                                onClose={() => setItem(null)}
+                            />
+                        )
+                    }
                 </div>
             )}
         </Query>
