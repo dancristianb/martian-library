05eb981357ff0bd173544a3ab1f8a263bf5bf542
Dan Bota <dan.cristian.bota@gmail.com>
2021-02-08 14:09:39 +0200

    Adding user info panel
    npx @hellsquirrel/create-gql-component create app/javascript/components/UserInfo
    implement UserInfo component based on https://evilmartians.com/chronicles/graphql-on-rails-2-updating-the-data\#adding-user-info-panel
    Dealing with cache section

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,12 +1,14 @@
 // app/javascript/packs/index.js
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 import Provider from '../components/Provider';
 import Library from '../components/Library';
+import UserInfo from "../components/UserInfo";
 
 render(
     <Provider>
-        <Library />
+        <UserInfo/>
+        <Library/>
     </Provider>,
     document.querySelector('#root')
 );
